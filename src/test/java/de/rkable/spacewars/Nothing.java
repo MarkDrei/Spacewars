d374c7bd3be96877edd7955e7a82d3648d44a279
MarkDrei <mark@mopedoversum.de>
2019-08-09 11:25:08 +0200

    initial setup

---
+++ src/test/java/de/rkable/spacewars/Nothing.java
@@ -0,0 +1,13 @@
+package de.rkable.spacewars;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class Nothing {
+
+	@Test
+	public void nothing() {
+		assertTrue(false);
+	}
+}
